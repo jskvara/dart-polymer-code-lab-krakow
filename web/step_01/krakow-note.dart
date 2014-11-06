39b09466f6c9b6a4d326515e26e7d19c62f71ba0
Jakub Škvára <jskvara@gmail.com>
2014-11-06 22:39:03 +0100

    Updated notes and note element

--- web/step_01/krakow-note.dart
+++ web/step_01/krakow-note.dart
@@ -1,5 +1,7 @@
 import 'package:polymer/polymer.dart';
 import 'note.dart';
+import 'dart:html';
+import 'package:paper_elements/paper_icon_button.dart';
 
 /**
  * A Polymer krakow-note element.
@@ -8,7 +10,12 @@ import 'note.dart';
 class KrakowNote extends PolymerElement {
 
   @published Note note;
-  
+  @published int index;
+
   KrakowNote.created() : super.created() {
+  }
+
+  void deleteNote(Event event, Object value, PaperIconButton target) {
+    this.fire('delete-note', detail: {'index': index});
   }
 }
--- web/step_01/krakow-notes.dart
+++ web/step_01/krakow-notes.dart
@@ -1,4 +1,5 @@
 import 'package:polymer/polymer.dart';
+import 'package:paper_elements/paper_input.dart';
 import 'dart:html';
 import 'note.dart';
 
@@ -17,20 +18,29 @@ class KrakowNotes extends PolymerElement {
     notes.add(new Note('aaa'));
   }
   
-  void createNote(Event event, Object noteText, Node target) {
-    String noteText = (target as InputElement).value;
+  void createNote(Event event, Object noteText, PaperInput target) {
+    String noteText = target.value;
     if (noteText.isNotEmpty) {
       addNote(noteText);
+      target.inputValue = '';
+      target.commit();
     }
   }
   
   void addNote(String noteText) {  
     notes.add(new Note(noteText)); 
-    $['note_add_form'].value = "";
     saveToLocalStorage();
   }
   
+  void deleteNote(Event event, var note) {
+    int index = note['index'];
+    notes.removeAt(index);
+  }
   
+  void deleteAll(Event event) {
+    notes.clear();
+  }
+
   void loadFromLoacalStorage() {
     if (window.localStorage[NOTES_LIST] != null) {
       notes = Note.fromJson(window.localStorage[NOTES_LIST]);
