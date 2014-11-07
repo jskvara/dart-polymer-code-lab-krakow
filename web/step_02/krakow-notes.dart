0c456f09d55f282fa961485805f96376830672f0
Jakub Škvára <jskvara@gmail.com>
2014-11-07 02:26:59 +0100

    Added step 2

---
+++ web/step_02/krakow-notes.dart
@@ -0,0 +1,12 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer krakow-notes element.
+ */
+@CustomTag('krakow-notes')
+class KrakowNotes extends PolymerElement {
+
+  KrakowNotes.created() : super.created() {
+  }
+
+}
